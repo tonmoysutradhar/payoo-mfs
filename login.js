0813bc4fcaf098a6c0e4d394f8c185e09e65cd33
tonmoysutradhar <tonmoykumar933@gmail.com>
2024-09-18 23:20:39 +0600

    add html and js file

---
+++ login.js
@@ -0,0 +1,13 @@
+console.log("Tonmoy Kumar Sutradhar");
+
+// step - 1 set event handler 
+document.getElementById("button-login").addEventListener("click", function(event){
+    // step - 2 set default behavior (prevent reloading browser)
+    event.preventDefault();
+    console.log("login button clicked");
+
+    // step - 3 get the phone number
+    const phoneNumber = document.getElementById("phone-number").value;
+    console.log(phoneNumber);
+    
+})
