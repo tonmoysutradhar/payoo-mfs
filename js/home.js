dbc809ef38c78df2f6673a9958bd6346e63a9410
tonmoysutradhar <tonmoykumar933@gmail.com>
2024-09-19 03:08:34 +0600

    add some home.js code

--- js/home.js
+++ js/home.js
@@ -1,1 +1,23 @@
-console.log("tom");
+/*
+S-1: add event handler
+prevent page reload after form submit
+
+S-2: get money to be added to the account balance
+*/
+
+// srep 1: add an event handler to the add money button inside the form
+document.getElementById("btn-add-money").addEventListener("click", function (event){
+    // prevent page reload after form submit
+    event.preventDefault();
+    console.log("adddddddddd");
+
+    // step 2: get money to be added to the account balance
+    const addMoneyInput = document.getElementById("input-add-money").value ;
+    console.log(addMoneyInput);
+    
+
+    // get the pin number provided
+    const pinNumberInput = document.getElementById("input-pin-number").value ;
+    console.log(pinNumberInput);
+    
+})
--- js/login.js
+++ js/login.js
@@ -6,8 +6,8 @@ document.getElementById("button-login").addEventListener("click", function(event
     // step - 3 get the phone number
     const phoneNumber = document.getElementById("phone-number").value;
     const pinNumber = document.getElementById("pin-number").value;
-    console.log(phoneNumber);
-    console.log(pinNumber);
+    // console.log(phoneNumber);
+    // console.log(pinNumber);
     
     // this is temporary
     if (phoneNumber === "555" && pinNumber == "1234") {
